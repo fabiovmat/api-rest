8a494b4460f05f5290741b0810247ddaf7a55d76
fabio <fabio.meihua08@gmail.com>
2020-08-04 11:23:49 -0300

    Tratamento de Erros Nivel de Banco de Dados

--- src/main/java/api/rest/ControleExcecoes.java
+++ src/main/java/api/rest/ControleExcecoes.java
@@ -1,7 +1,10 @@
 package api.rest;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,6 +47,50 @@ public class ControleExcecoes extends ResponseEntityExceptionHandler  {
 		objetoErro.setCode(status.value() + "==>" + status.getReasonPhrase());
 		
 		return new ResponseEntity<>(objetoErro, headers, status);
+		
+		
+	}
+	
+	
+	/*tratamento da maioria dos erros a nivel de banc ode dados*/
+	@ExceptionHandler({DataIntegrityViolationException.class, ConstraintViolationException.class, SQLException.class})
+	protected ResponseEntity<Object> handResponseEntity (Exception ex){
+		
+		
+		
+		String msg = "";
+		
+		if (ex instanceof DataIntegrityViolationException) {
+			
+		msg = ((DataIntegrityViolationException) ex).getCause().getCause().getMessage();    
+			
+		}else if (ex instanceof ConstraintViolationException) {
+			
+			msg = ((ConstraintViolationException) ex).getCause().getCause().getMessage();
+			
+		}else if (ex instanceof SQLException) {
+			
+			msg = ((SQLException) ex).getCause().getCause().getMessage();
+			
+		}
+		
+		
+		
+		else {
+			
+			msg = ex.getMessage();
+			
+		}
+		
+		
+		
+		ObjetoError objectError = new ObjetoError();
+		objectError.setError(msg);
+		objectError.setCode(HttpStatus.INTERNAL_SERVER_ERROR + "==>" + HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+		
+		return new ResponseEntity<>(objectError , HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		
 		
 		
 	}
